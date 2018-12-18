cbfafaf3ddb074038c4f396115504f29cb714d1f
Kamal Hossain <kamal.hossain@bjitgroup.com>
2018-12-18 16:34:38 +0600

    init

--- scripts/install-oracle-coherence.sh
+++ scripts/install-oracle-coherence.sh
@@ -3,4 +3,5 @@
 java -jar /vagrant/fmw/fmw_12.2.1.3.0_wls.jar -silent -responseFile /vagrant/fmw/response-file -invPtrLoc /vagrant/fmw/oraInst.loc
 rm -rf /vagrant/backup
 rm -rf /vagrant/ContentsXML
-rm -rf /vagrant/logs 
+rm -rf /vagrant/logs
+rm -rf /vagrant/locks
